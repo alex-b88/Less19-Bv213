e5d9a11ded6e63d785ea4ebd3ed2dc8a22146066
Lapshun Tetiana <lapshun.tatiana@gmail.com>
2023-08-17 20:30:30 +0300

    Added Outstaff definitions

--- Less19-Bv213/Outstaff.cpp
+++ Less19-Bv213/Outstaff.cpp
@@ -1,1 +1,16 @@
 #include "Outstaff.h"
+
+Outstaff::Outstaff()
+{
+}
+
+Outstaff::Outstaff(string name, string position, float salary)
+	:Employee(name,position,salary)
+{
+}
+
+void Outstaff::show() const
+{
+	cout << "Outstaff" << endl;
+		Employee::show();
+}
