e407dae8f2faf159e7410f57df4001fcb9f2fd71
Lapshun Tetiana <lapshun.tatiana@gmail.com>
2023-08-17 20:38:26 +0300

    Added Department definitions

--- Less19-Bv213/Department.cpp
+++ Less19-Bv213/Department.cpp
@@ -1,1 +1,16 @@
 #include "Department.h"
+
+void Department::addItem(Employee* obj)
+{
+	base.push_back(obj);
+}
+
+void Department::show() const
+{
+	for (int i = 0; i < base.size(); i++)
+	{
+		base[i]->show();
+
+			
+	}
+}
