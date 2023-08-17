979b1fb5a883e1bbabe3b282bee86feb182423cd
Lapshun Tetiana <lapshun.tatiana@gmail.com>
2023-08-17 20:19:48 +0300

    Added description Staff

--- Less19-Bv213/Staff.h
+++ Less19-Bv213/Staff.h
@@ -2,5 +2,11 @@
 #include "Employee.h"
 class Staff :  public Employee
 {
+protected:
+	float bonus;
+public:
+	Staff();
+	Staff(string name, string position, float salary, float bonus);
+	void show()const override;
 };
 
